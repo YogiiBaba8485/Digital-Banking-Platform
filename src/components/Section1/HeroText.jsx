f5145b4d9606fc608cd15422dd86d6b49953a060
Yogesh Jagdale <yogeshjagdale3254@gmail.com>
2025-11-08 07:40:15 +0530

    Initial commit - added project files

---
+++ src/components/Section1/HeroText.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const HeroText = () => {
+  return (
+     <div className='p-6'>
+          <h3 className='mb-7 text-6xl font-bold'>Prospective <br /><span className='text-gray-700  '>customer</span> <br /> Segmentation</h3>
+          <p className='text-xl font-medium text-gray-600'> Depending on customer satisfaction and access to banking products, potential target audience can be devided into three groups</p>
+     </div>
+  )
+}
+
+export default HeroText
---
+++ src/components/Section1/RightCardContent.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { ArrowRight } from 'lucide-react';
+const RightCardContent = (props) => {
+
+    console.log(props.color);
+  return (
+    <div className=' absolute top-0 left-0 h-full w-full p-8 flex flex-col justify-between '>
+                <h2 className='bg-white text-2xl font-semibold rounded-full h-12 w-12 flex justify-center items-center '>{props.id+1}</h2>
+                <div >
+                    <p className=' text-shadow-4xs text-lg  leading-snug text-white  mb-14 '> {props.intro} </p>
+                    <div className='flex justify-between'>
+                        <button style={{backgroundColor:props.color}} className='text-white font-medium px-8 py-2 rounded-full'>{props.tag}</button>
+                        <button className=' bg-blue-600 text-white font-medium px-4 py-2 rounded-full '><ArrowRight /> </button>
+                        </div>
+                </div>
+            </div>
+  )
+}
+
+export default RightCardContent
+
